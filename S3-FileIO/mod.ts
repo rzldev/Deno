3eb3915a9fde4a9c6681c097315d269c61a23ea6
rzldev <49812100+rzldev@users.noreply.github.com>
2020-08-28 14:18:03 +0700

    File I/O

---
+++ S3-FileIO/mod.ts
@@ -0,0 +1,98 @@
+import { join } from 'https://deno.land/std/path/mod.ts';
+import { BufReader } from 'https://deno.land/std/io/bufio.ts';
+import { parse } from 'https://deno.land/std/encoding/csv.ts';
+import * as _ from 'https://deno.land/x/lodash@4.17.15-es/lodash.js'
+
+/* 
+    Read a file 
+*/
+async function readFile() {
+    const data = await Deno.readTextFile('hello-world.txt');
+
+    console.log(data);
+}
+
+await readFile();
+
+/* 
+    Read a file Synchronously 
+*/
+const readFileSync = Deno.readTextFileSync("hello-world.txt");
+console.log(readFileSync);
+
+/* 
+    Read file from specific path 
+*/
+async function readFileWithPath() {
+    const path = join('books', 'new-books.txt');
+
+    const data = await Deno.readTextFile(path);
+
+    console.log(data);
+}
+
+await readFileWithPath();
+
+/* 
+    Read csv file 
+*/
+async function readCSVFile() {
+    const path = join('planets', 'kepler_exoplanets_nasa.csv');
+
+    const file = await Deno.open(path);
+    const bufReader = new BufReader(file);
+    const result = await parse(bufReader, {
+        header: true,
+        comment: '#',
+    });
+    Deno.close(file.rid);
+
+    console.log(result);
+}
+
+await readCSVFile();
+
+/*
+    Filter specific items from csv file
+*/
+interface Planet {
+    [ key: string ]: string
+}
+
+async function loadPlanets() {
+    const path = join('planets', 'kepler_exoplanets_nasa.csv');
+    const file = await Deno.open(path);
+    const bufReader = new BufReader(file);
+    const result = await parse(bufReader, {
+        header: true,
+        comment: '#',
+    });
+
+    Deno.close(file.rid);
+
+    const planets = (result as Array<Planet>).filter((planet) => {
+        const planetaryRadius = Number(planet['koi_prad']);
+        const stellarMass = Number(planet['koi_smass']);
+        const stellarRadius = Number(planet['koi_srad']);
+
+        return planet['koi_disposition'] === 'CONFIRMED'
+            && planetaryRadius > 0.5 && planetaryRadius < 1.5
+            && stellarMass > 0.78 && stellarMass < 1.04
+            && stellarRadius > 0.99 && stellarRadius < 1.01;
+    });
+
+    return planets.map((planet) => {
+        return _.pick(planet, [
+            'koi_prad',
+            'koi_smass',
+            'koi_srad',
+            'kepler_name',
+            'koi_count',
+            'koi_steff',
+        ]);
+    });
+}
+
+const habitablePlanets = await loadPlanets();
+console.log(`${habitablePlanets.length} habitable planets found!`);
+console.log(habitablePlanets);
